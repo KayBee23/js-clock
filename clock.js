6d4ba82b77e198881560c135386bfdec28ef69a4
KayBee23 <87897887+KayBee23@users.noreply.github.com>
2022-02-05 18:59:23 +0100

    Delete clock.js

--- clock.js
+++
@@ -1,8 +0,0 @@
-usTime[0].addEventListener("click", function () {
-  let timeOfDay = (h12 < 12)? 'AM' : 'PM';
-  let currentTime = `${h12}:` + `${m}:` + `${s}`+ `${timeOfDay}`;
-  time.innerHTML = `<p> ${currentTime}</p>`;
-  usTime[0].style.color = "#34ebd8";
-  euTime[0].style.color = "rgba(255, 255, 255, 0.1)";
-});
-
